c790a46c8c64226d55306e14ffbfd2916d352ce7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-23 16:46:42 -0600

    set up the defauolt URL option for the Devise mailer in development enviroment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
-
+    
     config.cache_store = :memory_store
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
@@ -40,6 +40,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
