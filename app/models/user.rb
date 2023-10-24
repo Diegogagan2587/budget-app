b5cbc3fdf35de2e46f9b17fa15e1e93bc0e2ab09
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-24 12:15:13 -0600

    Set relation: User has many groups and entities

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :groups
+  has_many :entities
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
