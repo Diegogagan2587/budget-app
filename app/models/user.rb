e87edd76679a6c02aa05d2ef7998d982804c11d3
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-23 23:05:52 -0600

    Add validations for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 end
