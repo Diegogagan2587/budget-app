fff2fd3e77d1a7314732c243c3a3789582429bf1
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-24 16:34:02 -0600

    Migrate to include relationship: Group has many entities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_24_025212) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_24_222732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +20,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_025212) do
     t.decimal "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
     t.index ["author_id"], name: "index_entities_on_author_id"
+    t.index ["group_id"], name: "index_entities_on_group_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -45,6 +47,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_025212) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "entities", "groups"
   add_foreign_key "entities", "users", column: "author_id"
   add_foreign_key "groups", "users"
 end
