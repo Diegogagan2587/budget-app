e78c7660ee8d9f4e9119f69b9deacf694ed6c0a5
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-24 16:38:08 -0600

    Fix Relationshilp between Models: Group has many entities

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,5 +1,6 @@
 class Entity < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  belongs_to :group
   validates :name, presence: true
   validates :amount, presence: true
   validates :author, presence: true
