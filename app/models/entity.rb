849979a706c00b449197ae1acb5349f45830c41e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-24 15:35:43 -0600

    Add validations in Entity model for name, amount and author

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,3 +1,6 @@
 class Entity < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  validates :name, presence: true
+  validates :amount, presence: true
+  validates :author, presence: true
 end
