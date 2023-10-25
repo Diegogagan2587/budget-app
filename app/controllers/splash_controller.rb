a7d97f6a3791a8e6698d1bae704017474d6753ad
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-24 22:21:14 -0600

    rails generate controller splash

---
+++ app/controllers/splash_controller.rb
@@ -0,0 +1,2 @@
+class SplashController < ApplicationController
+end
---
+++ spec/requests/splash_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Splashes", type: :request do
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end
