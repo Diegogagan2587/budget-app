96a949c4e6572f6d4390c37edbc8809fe585ee69
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-25 14:14:32 -0600

    Update transactions counter in Group after saving an Entity

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -4,4 +4,9 @@ class Entity < ApplicationRecord
   validates :name, presence: true
   validates :amount, presence: true
   validates :author, presence: true
+  after_save :update_group_transactions_counter
+
+  def update_group_transactions_counter
+    group.update(transactions_counter: group.entities.count)
+  end
 end
