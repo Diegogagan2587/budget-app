5189235309935f754458b0c8ac75bb31b863dd5b
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-25 00:33:51 -0600

    set groups#idnex as root, add routes for users, splash, groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'splash#index'
+  root to: 'groups#index'
+  resources :users
+  resources :splash, only: [:index]
+  resources :groups
 end
