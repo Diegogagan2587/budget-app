ac18e60710ce8fb40a6bbc4bff58e8d4f42d61f3
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-25 13:33:24 -0600

    Pass groups as @categories to categories page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,8 @@
 class GroupsController < ApplicationController
   before_action { redirect_to splash_index_path unless user_signed_in? }
-  def index; end
+  def index
+    @categories = current_user.groups
+  end
+
   def show; end
 end
