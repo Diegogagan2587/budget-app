3be5c9361166eaeb6db99ee0bb833712225bfcc0
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-25 13:51:53 -0600

    rails generate migration AddTransactionsCounterToGroups transaction_counter:integer and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_24_222732) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_25_194948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_24_222732) do
     t.string "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "transactions_counter"
     t.index ["user_id"], name: "index_groups_on_user_id"
   end
 
