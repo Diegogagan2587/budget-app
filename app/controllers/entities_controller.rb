864310dcb04e73db2166a6e4af07e536cb0d96c7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-25 22:49:42 -0600

    Implement EntitiesController actions for new and Create

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,2 +1,32 @@
 class EntitiesController < ApplicationController
+  before_action :authenticate_user!
+  def new
+    @user = current_user
+    @entity = Entity.new(author: @user)
+    respond_to do |format|
+      format.html do
+        render :new
+      end
+    end
+  end
+
+  def create
+    @entity = Entity.new(entity_params)
+    @entity.author = current_user
+    format.html do
+      if @entity.save
+        redirect_to entities_path
+      else
+        render :new
+      end
+    end
+
+    redirect_to entities_path
+  end
+
+  private
+
+  def entity_params
+    params.require(:entity).permit(:name, :amount)
+  end
 end
