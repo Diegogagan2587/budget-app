00ef7916b523860ab684cc3743cd61670a0ebc63
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-25 19:12:19 -0600

    add Group actions for New and create, and permit name and icons as parameters

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,5 +6,36 @@ class GroupsController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @group = Group.new(
+      user: current_user
+    )
+
+    respond_to do |format|
+      format.html do
+        render 'new'
+      end
+    end
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user = current_user
+
+    respond_to do |format|
+      format.html do
+        if @group.save
+          redirect_to groups_path
+        else
+          render 'new'
+        end
+      end
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
 end
