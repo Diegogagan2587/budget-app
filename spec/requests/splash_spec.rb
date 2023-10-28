787fb5dbd70f24b2d99645a09c029fd961d979ae
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-10-27 23:37:17 -0600

    Add test for splash controller

--- spec/requests/splash_spec.rb
+++ spec/requests/splash_spec.rb
@@ -1,7 +1,17 @@
+# We will implement request test for SplashController
 require 'rails_helper'
 
-RSpec.describe 'Splashes', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe SplashController, type: :request do
+  describe 'GET /splash' do
+    it 'should return 200' do
+      get '/splash'
+      expect(response).to have_http_status(200)
+    end
+    it 'should render the index template' do
+      get '/splash'
+      expect(response).to render_template(:index)
+    end
+
   end
+
 end
